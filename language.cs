f1077c21bbdb50ce7daeb34ac89b7abf95d550c0
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2019-02-14 13:24:40 -0500

    Update language.cs

--- language.cs
+++ language.cs
@@ -1,17 +1,3 @@
 # All of the projects listed on this landing page are written in C#, but since there techically isn't any **code** here, GitHub isn't listing the language on the repository page.
 #
 # By placing this file, with a ".cs" extension...and then adding a line to the .gitattributes file (since for some reason .cs is seen as SmallTalk), the language is correctly displayed.
-
-    © 2019 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
