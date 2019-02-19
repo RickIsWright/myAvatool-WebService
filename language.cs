9795c59d84bcd10fc8e8db6b5ded0f147a335476
Chris.Banwarth <development@aprettycoolprogram.com>
2019-02-19 15:40:16 -0500

    v0.9.0

--- language.cs
+++
@@ -1,3 +0,0 @@
-# All of the projects listed on this landing page are written in C#, but since there techically isn't any **code** here, GitHub isn't listing the language on the repository page.
-#
-# By placing this file, with a ".cs" extension...and then adding a line to the .gitattributes file (since for some reason .cs is seen as SmallTalk), the language is correctly displayed.
--- src/SSMHAVWS/obj/Release/Package/PackageTmp/Scheduling.asmx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebService Language="C#" CodeBehind="Scheduling.asmx.cs" Class="SSMHAVWS.Schedule" %>
